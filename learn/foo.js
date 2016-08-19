4d98326b1c319db8a07dd932f7463d5abf30c718
Segmond <segmond@gmail.com>
2016-08-18 23:48:16 -0400

    js foolery

--- learn/foo.js
+++ learn/foo.js
@@ -12,7 +12,26 @@ function foo() {
 	var y = 20;
 	eval("var y = 22;"); // get's ignored because we are in strict mode
 	console.log(y);
+	for (var i = 0; i < 5; i++) {
+		console.log('hello world');
+	}
 	
 }
 
 foo();
+
+function myfunc1(str) {
+	return str.toUpperCase();
+}
+
+console.log(myfunc1("booya"));
+
+function myfunc2() {
+	console.log("blah");
+}
+setInterval(myfunc2, 5000);
+
+setTimeout(function() {
+	console.log("mooo");
+}, 2000);
+
